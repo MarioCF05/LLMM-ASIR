8a2ed9ca72c87fa3d7fa93f0bbe2a6333c5be233
Mario Cordero Fernandez <mariocorderofdez@gmail.com>
2025-04-25 12:17:49 +0200

    Update script.js

--- temajava/DOM/Ejer_4/script.js
+++ temajava/DOM/Ejer_4/script.js
@@ -1,3 +1,11 @@
 console.log("Título:", document.title);
-console.log("Número de imágenes:", document.images.length);
-console.log("Número de enlaces:", document.links.length);
+
+console.log("Imágenes:");
+[...document.images].forEach((img, i) => {
+  console.log(`  ${i + 1}. ${img.src}`);
+});
+
+console.log("Enlaces:");
+[...document.links].forEach((link, i) => {
+  console.log(`  ${i + 1}. ${link.href}`);
+});
